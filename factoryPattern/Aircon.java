2f5b6f6867e7c37e8870e2b3d6bc163af2a67cf3
Lester0329 <72334101+Lester0329@users.noreply.github.com>
2023-10-17 23:57:17 +0800

    Create Aircon.java

---
+++ factoryPattern/Aircon.java
@@ -0,0 +1,22 @@
+package commandPattern;
+
+public class Aircon {
+    public String turnOn ()
+    {
+        return "Aircon is turned on!";
+    }
+
+    public String turnOff ()
+    {
+        return "Aircon is turned off!";
+    }
+
+    public String tempInc ()
+    {
+        return "Temperature increased!";
+    }
+    public String tempDec ()
+    {
+        return "Temperature decreased!";
+    }
+}
